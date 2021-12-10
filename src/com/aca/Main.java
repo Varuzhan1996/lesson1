fd5c1a9233d619ab3eee49dc9ac8a8b1d3112f21
Varuzhan1996 <varuzhanyeranosyan1996@mail.ru>
2021-12-10 21:38:20 +0400

    first homework

---
+++ src/com/aca/Main.java
@@ -0,0 +1,29 @@
+package com.aca;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //task 1
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please input first number");
+//        int firstElement = scanner.nextInt();
+//        int sum = firstElement +secondElement;
+//        System.out.println("sum is" + sum);
+//
+        //task 2
+        int a = 7;
+        int b = 8;
+        System.out.println(a!=b);
+        //task 3
+        int c = 25;
+        int d = 100;
+        System.out.println(c%d);
+        //task 4
+        int e = 8;
+        int f = 6;
+        System.out.println((e+f)/2);
+
+    }
+}
